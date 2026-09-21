d76bdab82aa5a6f7fe9eabe2e97cb3b49ff8e5f9
erlmek <erlm@ek.dk>
2026-09-21 11:29:59 +0200

    Kommune Postmapping

---
+++ src/main/java/org/example/regionkommunee25a/controller/KommuneRestController.java
@@ -0,0 +1,34 @@
+package org.example.regionkommunee25a.controller;
+
+import org.example.regionkommunee25a.model.Kommune;
+import org.example.regionkommunee25a.model.Region;
+import org.example.regionkommunee25a.repositories.KommuneRepository;
+import org.example.regionkommunee25a.repositories.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/kommuner")
+@CrossOrigin("*")
+public class KommuneRestController {
+
+    @Autowired
+    KommuneRepository kommuneRepository;
+
+    @GetMapping
+    List<Kommune> getRegioner() {
+        return kommuneRepository.findAll();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kommune postRegion(@RequestBody Kommune kommune) {
+        System.out.println(kommune);
+        return kommuneRepository.save(kommune);
+    }
+
+
+}
