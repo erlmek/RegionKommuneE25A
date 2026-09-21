ca7021d9f4627410cd01e4b29c1115b948dafc4f
erlmek <erlm@ek.dk>
2026-09-21 10:27:38 +0200

    Post mapping

--- src/main/java/org/example/regionkommunee25a/controller/RegionRestController.java
+++ src/main/java/org/example/regionkommunee25a/controller/RegionRestController.java
@@ -3,12 +3,15 @@ package org.example.regionkommunee25a.controller;
 import org.example.regionkommunee25a.model.Region;
 import org.example.regionkommunee25a.repositories.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/regioner")
+@CrossOrigin("*")
 public class RegionRestController {
 
     @Autowired
@@ -23,6 +26,18 @@ public class RegionRestController {
     List<Region> deleteRegion(@PathVariable String kode) {
         regionRepository.deleteById(kode);
         return regionRepository.findAll();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Region postRegion(@RequestBody Region region) {
+        System.out.println(region);
+        return regionRepository.save(region);
+    }
+
+    @PostMapping("/region2")
+    public ResponseEntity<String> postRegion2(@RequestParam String kode, @RequestParam String navn) {
+        return ResponseEntity.ok("Received kode: " + kode + ", navn: " + navn);
     }
 
 
